1aa0b7ec89ff71ab21bf3f1b0079b90561c647bd
nolanr1120 <nrice4@cps.edu>
2024-12-12 15:39:24 +0000

    v3

--- script.js
+++ script.js
@@ -2,43 +2,47 @@ let left = document.querySelector(".left")
 let right = document.querySelector(".right")
 let leftRight = document.querySelector(".leftRight")
 let text = document.querySelector("p")
-let safe = document.querySelector(".safe")
-let bank = document.querySelector(".bank")
-let computer = document.querySelector(".computer")
 let monster = document.querySelector(".monster")
 let inside = document.querySelector(".inside")
+let tyrant = document.querySelector(".tyrant")
+let axe = document.querySelector(".axe")
+let emoji = document.querySelector(".emoji")
 
-bank.style.display="none"
-safe.style.display="none"
-computer.style.display="none"
+
 monster.style.display="none"
 inside.style.display="none"
+tyrant.style.display="none"
+axe.style.display="none"
+emoji.style.display="none"
 
 
 
-
-// Rob a bank option
+// Go inside option
 
 left.addEventListener('click', function() {
     leftRight.style.display="none";
     left.style.display="none";
     right.style.display="none";
     inside.style.display="block";
-    text.innerHTML="You go inside the house and find and decide to stay for the night. But before you lay down in "
+    text.innerHTML="You go inside the house to stay for the night. But as soon as you enter the house, you're met with a big creature that's ready to kill you. </br></br> Use the srcoll wheel with run away or double click to fight it"
 });
 
 
 
-bank.addEventListener('dblclick', function() {
-    bank.style.display="none";
-    text.innerHTML="You decide to go further into the bank and find safe, but your caught by police."
-    safe.style.display="block"
+inside.addEventListener('dblclick', function() {
+    inside.style.display="none";
+    axe.style.display="block"
+    emoji.style.display="block"
+    text.innerHTML="You grab a nearby axe to fight the creature. Hover over the axe emoji to kill it."
+    
 });
 
-bank.addEventListener('wheel', function() {
-    bank.style.display="none";
+emoji.addEventListener('mousenter')
 
-    text.innerHTML="You decide to leave with the money that you have."
+inside.addEventListener('wheel', function() {
+    inside.style.display="none";
+    tyrant.style.display="block"
+    text.innerHTML="You try to run away but you're caught and killed."
 }); 
 
 // Other method
@@ -48,6 +52,5 @@ right.addEventListener('click', function() {
     left.style.display="none";
     right.style.display="none";
     monster.style.display="block"
-    text.innerHTML
 
 }); 
